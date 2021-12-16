753b9b19f79ff3e2f450bd85ad0c6c76f13ebd1e
p0pkern <petermanchristoph@gmail.com>
2021-12-15 19:13:58 -0500

    Updated styling of top navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./Navbar.css"
+
+/**
+ * The top navigation bar that appears on every page. Uses React Router to async
+ * navigate to every page in the app.
+ */
+const Navbar = () => {
+    return (
+        <ul className="navlist">
+            <li><Link   onClick={() => console.log("hello")}
+                        alt="Go to home page"
+                        to="/chrispetermanhome">Home</Link></li>
+            <li><Link   onClick={() => console.log("hello 2")}
+                        alt="A page of three projects"
+                        to="/chrispetermanhome/projects">Projects</Link></li>
+            <li><Link   onClick={() => console.log("hello 3")}
+                        alt="about the author"
+                        to="/chrispetermanhome/about">About</Link></li>
+            <li>Blog</li>
+        </ul>
+    )
+}
+
+export default Navbar
